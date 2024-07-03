7aff08c5bd63bafb46030b7683099af4e01d62c7
lepronet <lepronet85@gmail.com>
2024-07-03 12:27:08 +0100

    Add layout page and form provider component

---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const SignUp = (props: Props) => {
+  return (
+    <div className="flex-1 py-32 md:px-16 w-full">
+      <div className="flex flex-col h-full gap-3">
+        <SignUpFormProvider></SignUpFormProvider>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
