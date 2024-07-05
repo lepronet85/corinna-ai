f1955fe4c3dd9929ddfe35b290c299f114c00022
lepronet <lepronet85@gmail.com>
2024-07-05 11:20:48 +0100

    Add some components

--- src/app/schemas/auth.schema.ts
+++
@@ -1,9 +0,0 @@
-export type UserRegistrationProps = {
-  type: string;
-  fullname: string;
-  email: string;
-  confirmEmail: string;
-  password: string;
-  confirmPassword: string;
-  otp: string;
-};
--- src/components/forms/sign-up/form-provider.tsx
+++ src/components/forms/sign-up/form-provider.tsx
@@ -1,4 +1,5 @@
-import { AuthContextProvider } from "@/app/context/use-auth-context";
+import { AuthContextProvider } from "@/context/use-auth-context";
+import { useSignUpForm } from "@/hooks/sign-up/use-sign-up";
 import React from "react";
 import { FormProvider } from "react-hook-form";
 
@@ -7,11 +8,18 @@ type Props = {
 };
 
 const SignUpFormProvider = ({ children }: Props) => {
+  const { methods, onHandleSubmit, onGenerateOTP, loading } = useSignUpForm();
+
   return (
-    // <AuthContextProvider>
-    //   <FormProvider></FormProvider>
-    // </AuthContextProvider>
-    <></>
+    <AuthContextProvider>
+      <FormProvider {...methods}>
+        <form onSubmit={onHandleSubmit} className="h-full">
+          <div className="flex flex-col justify-between gap-3 h-full">
+            <Loader loading={loading}></Loader>
+          </div>
+        </form>
+      </FormProvider>
+    </AuthContextProvider>
   );
 };
 
