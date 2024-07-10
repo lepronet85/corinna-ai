9fffebba51137d9c74b5038ccf91554afd99be05
lepronet <lepronet85@gmail.com>
2024-07-10 17:00:05 +0100

    Add cards in landing page

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
 
-function NavBar() {
+const NavBar = () => {
   return (
     <div className="flex gap-5 justify-between items-center px-7 py-1 font-bold border-b border-solid border-zinc-100 leading-[154.5%] max-md:flex-wrap max-md:px-5">
       <div className="flex gap-1.5 justify-center self-stretch my-auto text-2xl tracking-tighter text-neutral-700">
@@ -34,6 +34,6 @@ function NavBar() {
       </Link>
     </div>
   );
-}
+};
 
 export default NavBar;
